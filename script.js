9af3d16a9b55cd777bc215142493c7c9418b04bf
Rachit Sharma <rachit995@rocketmail.com>
2021-07-18 01:05:27 +0530

    [ADD]: Added prod branch

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ template.innerHTML = `
       position: relative;
       transform-style: preserve-3d;
       animation-name: rotate-score-card;
-      animation-duration: 20s;
+      animation-duration: 10s;
       animation-timing-function: linear;
       animation-iteration-count: infinite;
     }
@@ -161,92 +161,109 @@ template.innerHTML = `
   </div>
 `;
 
-var element = document.createElement(`score-card-${Math.random().toString(36).slice(2)}`);
-var element_shadow_root = element.attachShadow({ mode: 'open' });
-element.shadowRoot.appendChild(template.content.cloneNode(true))
-var visibility = true;
-var zoom = true;
+class ScoreCard extends HTMLElement {
+  constructor () {
+    super();
+    this.attachShadow({
+      mode: 'open'
+    });
+    this.shadowRoot.appendChild(template.content.cloneNode(true))
+    this.visibility = true;
+    this.zoom = true;
 
-function toggleVisibility() {
-  visibility = !visibility;
-  const box = element.shadowRoot.querySelector('.score-card-box')
-  const toggleBtn = element.shadowRoot.querySelector('#visibility-button')
-  if (visibility) {
-    box.style.display = 'block';
-    toggleBtn.title = 'Hide Widget'
-    toggleBtn.innerHTML = `
+    function dragElement(elmnt) {
+      var pos1 = 0,
+        pos2 = 0,
+        pos3 = 0,
+        pos4 = 0;
+      elmnt.onmousedown = dragMouseDown;
+
+      function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+      }
+
+      function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+      }
+
+      function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+      }
+    }
+    dragElement(this.shadowRoot.querySelector('#score-card-wrapper'));
+  }
+
+  toggleVisibility() {
+    this.visibility = !this.visibility;
+    const box = this.shadowRoot.querySelector('.score-card-box')
+    const toggleBtn = this.shadowRoot.querySelector('#visibility-button')
+    if (this.visibility) {
+      box.style.display = 'block';
+      toggleBtn.title = 'Hide Widget'
+      toggleBtn.innerHTML = `
       <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21" />
       </svg>
       `;
-  } else {
-    box.style.display = 'none';
-    toggleBtn.title = 'Show Widget'
-    toggleBtn.innerHTML = `
+    } else {
+      box.style.display = 'none';
+      toggleBtn.title = 'Show Widget'
+      toggleBtn.innerHTML = `
       <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
       </svg>
       `;
+    }
   }
-}
 
-function toggleZoom() {
-  zoom = !zoom;
-  const info = element.shadowRoot.querySelector('#score-card-wrapper')
-  const toggleBtn = element.shadowRoot.querySelector('#zoom-button')
-  if (zoom) {
-    info.style.transform = 'scale(1)';
-    toggleBtn.title = 'Zoom in Widget'
-    toggleBtn.innerHTML = `
+  toggleZoom() {
+    this.zoom = !this.zoom;
+    const info = this.shadowRoot.querySelector('#score-card-wrapper')
+    const toggleBtn = this.shadowRoot.querySelector('#zoom-button')
+    if (this.zoom) {
+      info.style.transform = 'scale(1)';
+      toggleBtn.title = 'Zoom in Widget'
+      toggleBtn.innerHTML = `
       <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0zM10 7v3m0 0v3m0-3h3m-3 0H7" />
       </svg>
       `;
-  } else {
-    info.style.transform = 'scale(1.5)';
-    toggleBtn.title = 'Zoom out Widget'
-    toggleBtn.innerHTML = `
+    } else {
+      info.style.transform = 'scale(1.5)';
+      toggleBtn.title = 'Zoom out Widget'
+      toggleBtn.innerHTML = `
       <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0zM13 10H7" />
       </svg>
       `;
+    }
+  }
+
+  connectedCallback() {
+    this.shadowRoot.querySelector('#visibility-button').addEventListener('click', () => this.toggleVisibility())
+    this.shadowRoot.querySelector('#zoom-button').addEventListener('click', () => this.toggleZoom())
+  }
+
+  disconnectedCallback() {
+    this.shadowRoot.querySelector('#visibility-button').removeEventListener()
+    this.shadowRoot.querySelector('#zoom-button').removeEventListener()
   }
 }
 
 
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  elmnt.onmousedown = dragMouseDown;
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    document.onmousemove = elementDrag;
-  }
 
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
-
-dragElement(element.shadowRoot.querySelector('#score-card-wrapper'));
-
-element.shadowRoot.querySelector('#visibility-button').addEventListener('click', () => toggleVisibility())
-element.shadowRoot.querySelector('#zoom-button').addEventListener('click', () => toggleZoom())
-
-document.body.appendChild(element);
+window.customElements.define('score-card', ScoreCard)
